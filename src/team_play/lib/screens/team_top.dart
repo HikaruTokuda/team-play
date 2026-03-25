2553fe855b4afea48da2bc48518d33a7fdaf48c0
Tokuda Hikaru <hkrtkd@gmail.com>
2026-03-25 23:54:23 +0900

    チームTop描画時にダミーデータ読み込みの処理を追加

--- src/team_play/lib/screens/team_top.dart
+++ src/team_play/lib/screens/team_top.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:team_play/models/event.dart';
+import 'package:team_play/models/score.dart';
+import 'package:team_play/models/team.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class TeamTop extends StatefulWidget {
   const TeamTop({super.key});
@@ -9,6 +14,11 @@ class TeamTop extends StatefulWidget {
 
 class _TeamTop extends State<TeamTop> {
   @override
+  void initState() {
+    super.initState();
+    getEvents();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -16,5 +26,18 @@ class _TeamTop extends State<TeamTop> {
       ),
       body: Container(),
     );
+  }
+
+  Future<List<Event>> getEvents() async {
+    String loadData = await rootBundle.loadString('dummy/events.json');
+    final List<dynamic> rawData = jsonDecode(loadData);
+    List<Map<String, String>> eventList = rawData.map((item) {
+      return (item as Map<String, dynamic>).map((key, value){
+        return MapEntry(key, value.toString());
+      });
+    }).toList();
+    List<Event> events = eventList.map((i) => new Event.fromJson(i)).toList();
+    events.forEach((event) => print('eventName: ${event.eventName} eventDate: ${event.eventDate}'));
+    return events;
   }
 }
